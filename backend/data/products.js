4d522c7aaef372fb172a65570260f61d8adebd66
Vicky Hawley <49642354+vickyhawley@users.noreply.github.com>
2021-01-11 23:30:49 +0000

    Add files via upload

---
+++ backend/data/products.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+        name: 'Brecon',
+        image: '/images/brecon.png',
+        description: 'Bright landscape painting of Brecon Beacons',
+        price: 200,
+        countInStock: 1,
+        brand: 'Sian McGill',
+        category: 'Wales'
+    },
+    {
+        name: 'Three Cliffs',
+        image: '/images/threecliffs.png',
+        description: 'Bright landscape painting of Three Cliffs',
+        price: 230,
+        countInStock: 1,
+        brand: 'Sian McGill',
+        category: 'Wales'
+    },
+    {
+        name: 'Caswell',
+        image: '/images/caswell.png',
+        description: 'Bright landscape painting of Caswell',
+        price: 250,
+        countInStock: 1,
+        brand: 'Sian McGill',
+        category: 'Wales'
+    },
+]
+
+export default products
